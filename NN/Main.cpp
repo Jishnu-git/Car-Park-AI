7e5f717dfdfbff3198b21b118f31d37da0c4c685
Jishnu <jishnu30alamelu26@gmail.com>
2021-04-10 12:01:39 +0530

    Fixed critical logic flaws in NN

--- NN/Main.cpp
+++ NN/Main.cpp
@@ -3,7 +3,26 @@
 using namespace std;
 
 int main(void) {
-    vector<int> dims{3,3,1};
-    NN::NeuralNetwork brain(dims, NN::identity);
-    
+    try {
+        vector<int> dims{3,3,1};
+        NN::NeuralNetwork brain({1, 2, 1}, {NN::identity, NN::identity});
+        auto nodes = brain.getAllNodes();
+        for (size_t i = 0; i < nodes.size(); i++) {
+            std::cout << "Node " << i + 1 << ": ";
+            for (auto weight: nodes[i].getWeights()) {
+                std::cout << weight << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "\n\n";
+        brain.setInputs({1});
+        brain.computeOutputs();
+        nodes = brain.getAllNodes();
+        for (size_t i = 0; i < nodes.size(); i++) {
+            std::cout << "Node " << i + 1 << ": " << nodes[i].computeOutput() << std::endl;
+        }
+        std::cout << "\nFinal Output: " << brain.computeOutputs()[0];
+    } catch (NN::NNException e) {
+        std::cerr << e.describe();
+    }
 }
--- NN/NeuralNetwork.hpp
+++ NN/NeuralNetwork.hpp
@@ -31,11 +31,18 @@ namespace NN {
         return tanh(x);
     }
 
+    float ReLU(float x) {
+        return std::max(static_cast<float>(0), x);
+    }
+
+
+
     //NN
     class Node {
         std::vector<float> inputs;
         float output;
         std::vector<float> weights;
+        std::function<float (float)> activation;
 
         friend class NeuralNetwork;
     public:
@@ -43,10 +50,14 @@ namespace NN {
         Node(std::vector<float> inputs, std::vector<float> weights);
         Node(std::vector<float> inputs, float min, float max);
         Node(int size, float min, float max);
-        //Node(Node&);
+        Node(std::vector<float> inputs, std::vector<float> weights, std::function<float (float)> activation);
+        Node(std::vector<float> inputs, float min, float max, std::function<float (float)> activation);
+        Node(int size, float min, float max, std::function<float (float)> activation);
+        Node(const Node&);
         ~Node();
 
         void setInputs(std::vector<float> newInputs);
+        std::vector<float>& getWeights();
         const std::vector<float>& getInputs(); 
         float computeOutput();
         void mutate(float rate);
@@ -54,32 +65,28 @@ namespace NN {
 
     class NeuralNetwork {
         std::vector<Node> allNodes;
-        std::vector<Node*> inputNodes;
-        std::vector<Node*> outputNodes;
-        std::vector<Node*> hiddenNodes;
         std::vector<int> dims;
         std::unordered_map<int, std::vector<int>> mapping;
-        std::function<float (float)> activation; 
-
     public:
-        NeuralNetwork(std::vector<int> dims, std::function<float (float)> activation);
-        // NeuralNetwork(const NeuralNetwork&);
+        NeuralNetwork(std::vector<int> dims, std::vector<std::function<float (float)>> activation);
+        NeuralNetwork(const NeuralNetwork&);
         ~NeuralNetwork();
 
         const std::vector<Node>& getAllNodes();
-        const std::vector<Node*>& getInputNodes();
-        const std::vector<Node*>& getHiddenNodes();
-        const std::vector<Node*>& getOutputNodes();
+        const std::vector<Node>& getInputNodes();
+        const std::vector<Node>& getHiddenNodes();
+        const std::vector<Node>& getOutputNodes();
         std::vector<float> computeOutputs();
         void setInputs(std::vector<float> newInputs);
         void mutate(float rate);
         static std::vector<int> getRandomDims(int input, int output, int min, int max);
+        static std::vector<NeuralNetwork> mutateFromNN(const NeuralNetwork& nn, int population, float rate);
     };
 }
 
 namespace NN {
-#define MIN -10
-#define MAX 10
+#define MIN -1
+#define MAX 1
 
 float random(float min, float max) {
     return min + static_cast<float>(rng()) / static_cast<float>(rng.max() / (max - min));
@@ -96,21 +103,19 @@ bool fun (bool (*arg)(char *)) {
     return true;
 }
 
-NeuralNetwork::NeuralNetwork(std::vector<int> dims, std::function<float (float)> activation) : dims(dims), activation(activation) {
+NeuralNetwork::NeuralNetwork(std::vector<int> dims, std::vector<std::function<float (float)>> activation) : dims(dims) {
     if (dims.size() < 3) {
         throw except("Dimension of Neural Network is less than 3.");
     }
 
     for (size_t i = 0; i < dims[0]; i++) {
         allNodes.emplace_back(1);
-        inputNodes.push_back(&allNodes[i]);
     }
     std::vector<int> prevLayer(dims[0]);
     std::iota(prevLayer.begin(), prevLayer.end(), 0);
     for (size_t i = 1; i < dims.size() - 1; i++) {
         for (size_t j = 0; j < dims[i]; j++) {
-            allNodes.emplace_back(prevLayer.size(), MIN, MAX);
-            hiddenNodes.push_back(&(allNodes.back()));
+            allNodes.emplace_back(prevLayer.size(), MIN, MAX, activation[i - 1]);
             mapping[allNodes.size() - 1] = std::vector<int>(prevLayer);
         }
         int start = prevLayer.back() + 1;
@@ -119,60 +124,67 @@ NeuralNetwork::NeuralNetwork(std::vector<int> dims, std::function<float (float)>
     }
 
     for (size_t i = 0; i < dims.back(); i++) {
-        allNodes.emplace_back(prevLayer.size(), MIN, MAX);
-        outputNodes.push_back(&(allNodes.back()));
+        allNodes.emplace_back(prevLayer.size(), MIN, MAX, activation.back());
         mapping[allNodes.size() - 1] = std::vector<int>(prevLayer);
     }
 }
 
+NeuralNetwork::NeuralNetwork(const NeuralNetwork& nn) {
+    dims = nn.dims;
+    allNodes = nn.allNodes;
+    mapping = nn.mapping;
+}
+
 NeuralNetwork::~NeuralNetwork() {
     allNodes.clear();
-    inputNodes.clear();
-    hiddenNodes.clear();
-    outputNodes.clear();
     mapping.clear();
 }
 
 void NeuralNetwork::mutate(float rate) {
-    for (size_t i = inputNodes.size(); i < allNodes.size(); i++) {
+    for (size_t i = dims[0]; i < allNodes.size(); i++) {
         allNodes[i].mutate(rate);
     }
 }
 
 std::vector<float> NeuralNetwork::computeOutputs() {
-    for (size_t i = 0; i < inputNodes.size(); i++) {
+    for (size_t i = 0; i < dims[0]; i++) {
         allNodes[i].output = allNodes[i].inputs[0];
     }
 
-    for (size_t i = 0; i < hiddenNodes.size(); i++) {
-        std::vector<float> inputs;
-        for (auto j : mapping[i + dims[0]]) {
-            inputs.push_back(allNodes[j].output);
+    int offset = dims[0];
+    for (size_t i = 1; i < dims.size() - 1; i++) {
+        for (size_t j = 0; j < dims[i]; j++) {
+            std::vector<float> inputs;
+            for (auto k : mapping[j + offset]) {
+                inputs.push_back(allNodes[k].output);
+            }
+            allNodes[j + offset].setInputs(inputs);
+            allNodes[j + offset].computeOutput();
         }
-        allNodes[i + dims[0]].setInputs(inputs);
-        allNodes[i + dims[0]].computeOutput();
+        offset += dims[i];
     }
+    
 
     std::vector<float> output;
-    for (size_t i = 0; i < outputNodes.size(); i++) {
+    for (size_t i = 0; i < dims.back(); i++) {
         std::vector<float> inputs;
-        for (auto j : mapping[i + dims[0]]) {
+        for (auto j : mapping[i + offset]) {
             inputs.push_back(allNodes[j].output);
         }
-        allNodes[i + dims[0] + dims[1]].setInputs(inputs);
-        allNodes[i + dims[0] + dims[1]].output = activation(allNodes[i + dims[0] + dims[1]].computeOutput());
-        output.push_back(allNodes[i + dims[0] + dims[1]].output);
+        allNodes[i + offset].setInputs(inputs);
+        allNodes[i + offset].computeOutput();
+        output.push_back(allNodes[i + offset].output);
     }
 
     return output;
 }
 
 void NeuralNetwork::setInputs(std::vector<float> newInputs) {
-    if (inputNodes.size() != newInputs.size()) {
+    if (dims[0] != newInputs.size()) {
         throw except("Input vector dimension does not match the dimensions of neural network");
         return;
     }
-    for (size_t i = 0; i < inputNodes.size(); i++) {
+    for (size_t i = 0; i < dims[0]; i++) {
         allNodes[i].setInputs(std::vector<float>{newInputs[i]});
     }
 }
@@ -188,21 +200,32 @@ std::vector<int> NeuralNetwork::getRandomDims(int input, int output, int min = 3
     return dims;
 }
 
+std::vector<NeuralNetwork> NeuralNetwork::mutateFromNN(const NeuralNetwork& nn, int population, float rate = 0.1) {
+    std::vector<NeuralNetwork> nextGen;
+    nextGen.push_back(nn);
+    for (int _ = 1; _ < population; _++) {
+        NeuralNetwork newNN(nn);
+        newNN.mutate(rate);
+        nextGen.push_back(newNN);
+    }
+    return nextGen;
+}
+
 
 const std::vector<Node>& NeuralNetwork::getAllNodes() {
     return allNodes;
 }
 
-const std::vector<Node*>& NeuralNetwork::getInputNodes() {
-    return inputNodes;
+const std::vector<Node>& NeuralNetwork::getInputNodes() {
+    return std::vector<Node>(allNodes.begin(), allNodes.begin() + dims[0]);
 }
 
-const std::vector<Node*>& NeuralNetwork::getHiddenNodes() {
-    return hiddenNodes;
+const std::vector<Node>& NeuralNetwork::getHiddenNodes() {
+    return std::vector<Node>(allNodes.begin() + dims[0], allNodes.begin() + dims[1]);
 }
 
-const std::vector<Node*>& NeuralNetwork::getOutputNodes() {
-    return outputNodes;
+const std::vector<Node>& NeuralNetwork::getOutputNodes() {
+    return std::vector<Node>(allNodes.begin() + dims[1], allNodes.begin() + dims[2]);
 }
 
 
@@ -210,24 +233,31 @@ const std::vector<Node*>& NeuralNetwork::getOutputNodes() {
 ====================================Node===================================
 ===========================================================================*/
 
-Node::Node(int size) : inputs(size, 0), weights(size, 1) {}
+Node::Node(int size) : inputs(size, 0), weights(size, 1), activation(identity) {}
 
-// Node::Node(Node& newNode) {
-//     inputs = newNode.inputs;
-//     weights = newNode.weights;
-//     output = newNode.output;
-// }
+Node::Node(const Node& newNode) {
+    inputs = newNode.inputs;
+    weights = newNode.weights;
+    output = newNode.output;
+    activation = newNode.activation;
+}
 
-Node::Node(std::vector<float> inputs, std::vector<float> weights) : inputs(inputs), weights(weights) {}
+Node::Node(std::vector<float> inputs, std::vector<float> weights) : inputs(inputs), weights(weights), activation(identity) {}
 
-Node::Node(std::vector<float> inputs, float min, float max) : inputs(inputs) {
+Node::Node(std::vector<float> inputs, std::vector<float> weights, std::function<float (float)> activation) : inputs(inputs), weights(weights), activation(activation) {}
+
+Node::Node(std::vector<float> inputs, float min, float max) : Node(inputs, min, max, identity) {}
+
+Node::Node(std::vector<float> inputs, float min, float max, std::function<float (float)> activation) : inputs(inputs), activation(activation) {
     weights.clear();
     for (size_t i = 0; i < inputs.size(); i++) {
         weights.push_back(random(min, max));
     }
 }
 
-Node::Node(int size, float min, float max) : inputs(size) {
+Node::Node(int size, float min, float max) : Node(size, min, max, identity) {}
+
+Node::Node(int size, float min, float max, std::function<float (float)> activation) : inputs(size), activation(activation) {
     weights.clear();
     for (size_t i = 0; i < inputs.size(); i++) {
         weights.push_back(random(min, max));
@@ -243,6 +273,10 @@ void Node::setInputs(std::vector<float> newInputs) {
     inputs = newInputs;
 }
 
+std::vector<float>& Node::getWeights(){
+    return weights;
+}
+
 const std::vector<float>& Node::getInputs() {
     return inputs;
 }
@@ -252,6 +286,7 @@ float Node::computeOutput() {
     for (size_t i = 0; i < inputs.size(); i++) {
         out += weights[i] * inputs[i];
     }
+    out = activation(out);
     output = out;
     return out;
 }
@@ -259,7 +294,7 @@ float Node::computeOutput() {
 void Node::mutate(float rate) {
     for (size_t i = 0; i < weights.size(); i++) {
         if (random(0, 1) < rate) {
-            weights[i] *= random(-2, 2);
+            weights[i] += random(-1, 1);
         }
     }
 }
